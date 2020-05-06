c3e1b0113b45989d5aa4d5c061efc3d425e0d298
Yukino Hayakawa <tennencoll@gmail.com>
2020-05-06 21:02:16 +0800

    add test for task graph

---
+++ TestExecutorTaskflow/main.cpp
@@ -0,0 +1,33 @@
+﻿#include <gtest/gtest.h>
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+
+#include <Usagi/Module/Common/ExecutorTaskflow/Graph.hpp>
+
+using namespace usagi;
+
+TEST(TestExecutorTaskflow, TaskGraphTopologicalSort)
+{
+    Graph g;
+
+    g.add_edge(2, 4);
+    g.add_edge(2, 7);
+    g.add_edge(2, 8);
+    g.add_edge(4, 3);
+    g.add_edge(4, 10);
+    g.add_edge(7, 10);
+    g.add_edge(7, 1);
+    g.add_edge(7, 9);
+    g.add_edge(8, 9);
+    g.add_edge(3, 6);
+    g.add_edge(10, 6);
+    g.add_edge(1, 6);
+    g.add_edge(9, 6);
+
+    TopologicalSort ts { g };
+
+    ts.sort(2);
+}
