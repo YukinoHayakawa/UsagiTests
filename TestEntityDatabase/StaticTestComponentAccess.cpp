5301bae8ddcede42695cba468f533f81ef12efe0
Yukino Hayakawa <tennencoll@gmail.com>
2020-05-17 15:49:29 +0800

    update component filter tests

--- TestEntityDatabase/StaticTestComponentAccess.cpp
+++ TestEntityDatabase/StaticTestComponentAccess.cpp
@@ -22,16 +22,16 @@ struct SystemAccessTraitTest
     using WriteAccess = ComponentFilter<ComponentA>;
 };
 static_assert(detail::ComponentWriteMaskBitPresent<
-    SystemAccessTraitTest, ComponentA>::value
-);
+    SystemAccessTraitTest, ComponentA
+>::value);
 
 // TEST: Write access implies read access
-static_assert(SystemHasComponentReadAccess<
+static_assert(SystemCanReadComponent<
     SystemAccessTraitTest, ComponentA
 >);
 
 // TEST: Explicit write access mask
-static_assert(SystemHasComponentWriteAccess<
+static_assert(SystemCanWriteComponent<
     SystemAccessTraitTest, ComponentA
 >);
 
@@ -41,7 +41,7 @@ static_assert(std::is_same_v<ComponentReferenceType<
 >, ComponentA &>);
 
 // TEST: No read access
-static_assert(!SystemHasComponentReadAccess<
+static_assert(!SystemCanReadComponent<
     SystemAccessTraitTest, ComponentB
 >);
 
@@ -51,7 +51,49 @@ static_assert(std::is_same_v<ComponentReferenceType<
 >, void>);
 
 // TEST: No write access
-static_assert(!SystemHasComponentWriteAccess<
+static_assert(!SystemCanWriteComponent<
+    SystemAccessTraitTest, ComponentB
+>);
+
+// TEST: Access check by filter
+static_assert(CanWriteComponentsByFilter<
+    ComponentAccessSystemAttribute<SystemAccessTraitTest>,
+    ComponentFilter<ComponentA>
+>);
+static_assert(!CanWriteComponentsByFilter<
+    ComponentAccessSystemAttribute<SystemAccessTraitTest>,
+    ComponentFilter<ComponentB>
+>);
+static_assert(CanReadComponentsByFilter<
+    ComponentAccessSystemAttribute<SystemAccessTraitTest>,
+    ComponentFilter<ComponentA>
+>);
+static_assert(!CanReadComponentsByFilter<
+    ComponentAccessSystemAttribute<SystemAccessTraitTest>,
+    ComponentFilter<ComponentB>
+>);
+
+// TEST: Effective component filter
+static_assert(std::is_same_v<
+    ExplicitSystemComponentAccessRead<SystemAccessTraitTest>,
+    ComponentFilter<>
+>);
+static_assert(std::is_same_v<
+    ExplicitSystemComponentAccessWrite<SystemAccessTraitTest>,
+    ComponentFilter<ComponentA>
+>);
+static_assert(std::is_same_v<
+    SystemExplicitComponentFilter<SystemAccessTraitTest>,
+    ComponentFilter<ComponentA>
+>);
+
+// TEST: Accesses component via WriteAccess filter
+static_assert(SystemExplicitComponentAccess<
+    SystemAccessTraitTest, ComponentA
+>);
+
+// TEST: Not accessing a component
+static_assert(!SystemExplicitComponentAccess<
     SystemAccessTraitTest, ComponentB
 >);
 
@@ -64,12 +106,12 @@ struct SystemAccessTraitTest2
 };
 
 // TEST: Write access implies read access
-static_assert(SystemHasComponentReadAccess<
+static_assert(SystemCanReadComponent<
     SystemAccessTraitTest2, ComponentA
 >);
 
 // TEST: Explicit read access mask
-static_assert(SystemHasComponentReadAccess<
+static_assert(SystemCanReadComponent<
     SystemAccessTraitTest2, ComponentB
 >);
 
@@ -77,6 +119,44 @@ static_assert(SystemHasComponentReadAccess<
 static_assert(std::is_same_v<ComponentReferenceType<
     SystemAccessTraitTest2, ComponentB
 >, const ComponentB &>);
+
+// TEST: Access check by filter
+static_assert(CanWriteComponentsByFilter<
+    ComponentAccessSystemAttribute<SystemAccessTraitTest2>,
+    ComponentFilter<ComponentA>
+>);
+static_assert(!CanWriteComponentsByFilter<
+    ComponentAccessSystemAttribute<SystemAccessTraitTest2>,
+    ComponentFilter<ComponentB>
+>);
+static_assert(CanReadComponentsByFilter<
+    ComponentAccessSystemAttribute<SystemAccessTraitTest2>,
+    ComponentFilter<ComponentA, ComponentB>
+>);
+
+// TEST: Effective component filter
+static_assert(std::is_same_v<
+    ExplicitSystemComponentAccessRead<SystemAccessTraitTest2>,
+    ComponentFilter<ComponentA, ComponentB>
+>);
+static_assert(std::is_same_v<
+    ExplicitSystemComponentAccessWrite<SystemAccessTraitTest2>,
+    ComponentFilter<ComponentA>
+>);
+static_assert(std::is_same_v<
+    SystemExplicitComponentFilter<SystemAccessTraitTest2>,
+    ComponentFilter<ComponentA, ComponentB>
+>);
+
+// TEST: Accesses component via WriteAccess filter
+static_assert(SystemExplicitComponentAccess<
+    SystemAccessTraitTest2, ComponentA
+>);
+
+// TEST: Not accessing a component
+static_assert(SystemExplicitComponentAccess<
+    SystemAccessTraitTest2, ComponentB
+>);
 
 // ============================================================================
 
@@ -86,7 +166,7 @@ struct SystemAccessTraitTest3
 };
 
 // TEST: Read access does not imply write access
-static_assert(!SystemHasComponentWriteAccess<
+static_assert(!SystemCanWriteComponent<
     SystemAccessTraitTest3, ComponentA
 >);
 
@@ -100,6 +180,44 @@ static_assert(std::is_same_v<ComponentReferenceType<
     SystemAccessTraitTest3, ComponentB
 >, void>);
 
+// TEST: Access check by filter
+static_assert(!CanWriteComponentsByFilter<
+    ComponentAccessSystemAttribute<SystemAccessTraitTest3>,
+    ComponentFilter<ComponentA, ComponentB>
+>);
+static_assert(CanReadComponentsByFilter<
+    ComponentAccessSystemAttribute<SystemAccessTraitTest3>,
+    ComponentFilter<ComponentA>
+>);
+static_assert(!CanReadComponentsByFilter<
+    ComponentAccessSystemAttribute<SystemAccessTraitTest3>,
+    ComponentFilter<ComponentB>
+>);
+
+// TEST: Effective component filter
+static_assert(std::is_same_v<
+    ExplicitSystemComponentAccessRead<SystemAccessTraitTest3>,
+    ComponentFilter<ComponentA>
+>);
+static_assert(std::is_same_v<
+    ExplicitSystemComponentAccessWrite<SystemAccessTraitTest3>,
+    ComponentFilter<>
+>);
+static_assert(std::is_same_v<
+    SystemExplicitComponentFilter<SystemAccessTraitTest3>,
+    ComponentFilter<ComponentA>
+>);
+
+// TEST: Accesses component via WriteAccess filter
+static_assert(SystemExplicitComponentAccess<
+    SystemAccessTraitTest3, ComponentA
+>);
+
+// TEST: Not accessing a component
+static_assert(!SystemExplicitComponentAccess<
+    SystemAccessTraitTest3, ComponentB
+>);
+
 // ============================================================================
 
 struct SystemAccessTraitTest4
@@ -107,19 +225,53 @@ struct SystemAccessTraitTest4
     using ReadAllAccess = void;
 };
 
-static_assert(SystemHasComponentReadAccess<
+static_assert(SystemCanReadComponent<
     SystemAccessTraitTest4, ComponentA
 >);
 
-static_assert(SystemHasComponentReadAccess<
+static_assert(SystemCanReadComponent<
     SystemAccessTraitTest4, ComponentB
 >);
 
-static_assert(!SystemHasComponentWriteAccess<
+static_assert(!SystemCanWriteComponent<
     SystemAccessTraitTest4, ComponentA
 >);
 
-static_assert(!SystemHasComponentWriteAccess<
+static_assert(!SystemCanWriteComponent<
+    SystemAccessTraitTest4, ComponentB
+>);
+
+// TEST: Access check by filter
+static_assert(!CanWriteComponentsByFilter<
+    ComponentAccessSystemAttribute<SystemAccessTraitTest4>,
+    ComponentFilter<ComponentA, ComponentB>
+>);
+static_assert(CanReadComponentsByFilter<
+    ComponentAccessSystemAttribute<SystemAccessTraitTest4>,
+    ComponentFilter<ComponentA, ComponentB>
+>);
+
+// TEST: Effective component filter
+static_assert(std::is_same_v<
+    ExplicitSystemComponentAccessRead<SystemAccessTraitTest4>,
+    ComponentFilter<>
+>);
+static_assert(std::is_same_v<
+    ExplicitSystemComponentAccessWrite<SystemAccessTraitTest4>,
+    ComponentFilter<>
+>);
+static_assert(std::is_same_v<
+    SystemExplicitComponentFilter<SystemAccessTraitTest4>,
+    ComponentFilter<>
+>);
+
+// TEST: Accesses component via WriteAccess filter
+static_assert(!SystemExplicitComponentAccess<
+    SystemAccessTraitTest4, ComponentA
+>);
+
+// TEST: Not accessing a component
+static_assert(!SystemExplicitComponentAccess<
     SystemAccessTraitTest4, ComponentB
 >);
 
@@ -130,19 +282,53 @@ struct SystemAccessTraitTest5
     using WriteAllAccess = void;
 };
 
-static_assert(SystemHasComponentReadAccess<
+static_assert(SystemCanReadComponent<
     SystemAccessTraitTest5, ComponentA
 >);
 
-static_assert(SystemHasComponentReadAccess<
+static_assert(SystemCanReadComponent<
     SystemAccessTraitTest5, ComponentB
 >);
 
-static_assert(SystemHasComponentWriteAccess<
+static_assert(SystemCanWriteComponent<
     SystemAccessTraitTest5, ComponentA
 >);
 
-static_assert(SystemHasComponentWriteAccess<
+static_assert(SystemCanWriteComponent<
+    SystemAccessTraitTest5, ComponentB
+>);
+
+// TEST: Access check by filter
+static_assert(CanWriteComponentsByFilter<
+    ComponentAccessSystemAttribute<SystemAccessTraitTest5>,
+    ComponentFilter<ComponentA, ComponentB>
+>);
+static_assert(CanReadComponentsByFilter<
+    ComponentAccessSystemAttribute<SystemAccessTraitTest5>,
+    ComponentFilter<ComponentA, ComponentB>
+>);
+
+// TEST: Effective component filter
+static_assert(std::is_same_v<
+    ExplicitSystemComponentAccessRead<SystemAccessTraitTest5>,
+    ComponentFilter<>
+>);
+static_assert(std::is_same_v<
+    ExplicitSystemComponentAccessWrite<SystemAccessTraitTest5>,
+    ComponentFilter<>
+>);
+static_assert(std::is_same_v<
+    SystemExplicitComponentFilter<SystemAccessTraitTest5>,
+    ComponentFilter<>
+>);
+
+// TEST: Accesses component via WriteAccess filter
+static_assert(!SystemExplicitComponentAccess<
+    SystemAccessTraitTest5, ComponentA
+>);
+
+// TEST: Not accessing a component
+static_assert(!SystemExplicitComponentAccess<
     SystemAccessTraitTest5, ComponentB
 >);
 
---
+++ TestEntityDatabase/StaticTestComponentFilter.cpp
@@ -0,0 +1,46 @@
+﻿#include <type_traits>
+
+#include <Usagi/Game/detail/ComponentFilter.hpp>
+
+namespace usagi
+{
+namespace
+{
+struct ComponentA
+{
+};
+
+struct ComponentB
+{
+};
+
+struct ComponentC
+{
+};
+
+struct ComponentD
+{
+};
+}
+
+static_assert(std::is_same_v<
+    UniqueComponents<ComponentA, ComponentA, ComponentA>,
+    ComponentFilter<ComponentA>
+>);
+
+static_assert(std::is_same_v<
+    FilterConcatenatedT<
+        ComponentFilter<ComponentA, ComponentB>,
+        ComponentFilter<ComponentA, ComponentC>
+    >,
+    ComponentFilter<ComponentA, ComponentB, ComponentA, ComponentC>
+>);
+
+static_assert(std::is_same_v<
+    FilterDeduplicatedT<FilterConcatenatedT<
+        ComponentFilter<ComponentA, ComponentB>,
+        ComponentFilter<ComponentD, ComponentA, ComponentC>
+    >>,
+    ComponentFilter<ComponentA, ComponentB, ComponentD, ComponentC>
+>);
+}
