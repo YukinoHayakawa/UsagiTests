5d27c24a9a60040876525b64684b3b7df4cb4cfe
Yukino Hayakawa <tennencoll@gmail.com>
2020-05-19 20:03:58 +0800

    add tests for entity db

---
+++ TestEntityDatabase/StaticTestSystem.cpp
@@ -0,0 +1,47 @@
+﻿#include <Usagi/Game/detail/ComponentAccessSystemAttribute.hpp>
+#include <Usagi/Game/detail/ComponentFilter.hpp>
+
+namespace usagi
+{
+namespace
+{
+struct ComponentA
+{
+};
+
+struct ComponentB
+{
+};
+
+struct ComponentC
+{
+};
+
+struct ComponentD
+{
+};
+
+struct System1
+{
+    using ReadAccess = ComponentFilter<ComponentA>;
+    using WriteAccess = ComponentFilter<ComponentA, ComponentB>;
+};
+
+struct System2
+{
+    using ReadAllAccess = void;
+    using WriteAccess = ComponentFilter<ComponentA, ComponentB, ComponentC>;
+};
+
+struct System3
+{
+    using ReadAccess = ComponentFilter<ComponentC, ComponentA, ComponentD>;
+    using WriteAllAccess = void;
+};
+
+static_assert(std::is_same_v<
+    SystemComponentUsage<System1, System2, System3>,
+    ComponentFilter<ComponentA, ComponentB, ComponentC, ComponentD>
+>);
+}
+}
