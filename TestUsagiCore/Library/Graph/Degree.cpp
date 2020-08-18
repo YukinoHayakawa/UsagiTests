690964a8a4a3f24921d0799f05d1e2a024e7f59b
Yukino Hayakawa <tennencoll@gmail.com>
2020-08-18 14:12:49 +0800

    update graph tests

---
+++ TestUsagiCore/Library/Graph/Degree.cpp
@@ -0,0 +1,41 @@
+﻿#include <gtest/gtest.h>
+
+#include <Usagi/Library/Graph/Degree.hpp>
+
+#include "TestGraphAdjMatFixed.hpp"
+
+TEST(GraphAlgorithms, InDegrees)
+{
+    constexpr auto g = test_graph();
+    constexpr auto in_deg = in_degree(g);
+
+    static_assert(in_deg[0] == 0);
+    static_assert(in_deg[1] == 1);
+    static_assert(in_deg[2] == 0);
+    static_assert(in_deg[3] == 1);
+    static_assert(in_deg[4] == 1);
+    static_assert(in_deg[5] == 0);
+    static_assert(in_deg[6] == 4);
+    static_assert(in_deg[7] == 1);
+    static_assert(in_deg[8] == 1);
+    static_assert(in_deg[9] == 2);
+    static_assert(in_deg[10] == 2);
+}
+
+TEST(GraphAlgorithms, OutDegrees)
+{
+    constexpr auto g = test_graph();
+    constexpr auto out_deg = out_degree(g);
+
+    static_assert(out_deg[0] == 0);
+    static_assert(out_deg[1] == 1);
+    static_assert(out_deg[2] == 3);
+    static_assert(out_deg[3] == 1);
+    static_assert(out_deg[4] == 2);
+    static_assert(out_deg[5] == 0);
+    static_assert(out_deg[6] == 0);
+    static_assert(out_deg[7] == 3);
+    static_assert(out_deg[8] == 1);
+    static_assert(out_deg[9] == 1);
+    static_assert(out_deg[10] == 1);
+}
---
+++ TestUsagiCore/Library/Graph/FindPath.cpp
@@ -0,0 +1,85 @@
+﻿#include <gtest/gtest.h>
+
+#include <Usagi/Library/Graph/AdjacencyList.hpp>
+#include <Usagi/Library/Graph/FindPath.hpp>
+#include <Usagi/Library/Graph/ExtractPath.hpp>
+
+using namespace usagi::graph;
+
+TEST(GraphAlgorithms, LongestPathDynamic)
+{
+    AdjacencyList g { 6 };
+
+    g.add_edge(0, 1, 5);
+    g.add_edge(0, 2, 3);
+    g.add_edge(1, 3, 6);
+    g.add_edge(1, 2, 2);
+    g.add_edge(2, 4, 4);
+    g.add_edge(2, 5, 2);
+    g.add_edge(2, 3, 7);
+    g.add_edge(3, 5, 1);
+    g.add_edge(3, 4, -1);
+    g.add_edge(4, 5, -2);
+
+    const auto [prev, dist] = longest_path_dag(g, 1);
+    EXPECT_EQ(dist[5], 10);
+
+    auto path = path_to_stack<AdjacencyList>(prev, 1, 5);
+
+    EXPECT_EQ(path.top(), 1);
+    path.pop();
+    EXPECT_EQ(path.top(), 2);
+    path.pop();
+    EXPECT_EQ(path.top(), 3);
+    path.pop();
+    EXPECT_EQ(path.top(), 5);
+    path.pop();
+    EXPECT_TRUE(path.empty());
+
+    const auto path_vec = path_to_array<AdjacencyList>(prev, 1, 5);
+
+    EXPECT_EQ(path_vec.size(), 4);
+    EXPECT_EQ(path_vec[0], 1);
+    EXPECT_EQ(path_vec[1], 2);
+    EXPECT_EQ(path_vec[2], 3);
+    EXPECT_EQ(path_vec[3], 5);
+}
+
+TEST(GraphAlgorithms, ShortestPathDynamic)
+{
+    AdjacencyList g { 6 };
+
+    g.add_edge(0, 1, 5);
+    g.add_edge(0, 2, 3);
+    g.add_edge(1, 3, 6);
+    g.add_edge(1, 2, 2);
+    g.add_edge(2, 4, 4);
+    g.add_edge(2, 5, 2);
+    g.add_edge(2, 3, 7);
+    g.add_edge(3, 5, 1);
+    g.add_edge(3, 4, -1);
+    g.add_edge(4, 5, -2);
+
+    const auto [prev, dist] = shortest_path_dag(g, 1);
+    EXPECT_EQ(dist[5], 3);
+
+    auto path = path_to_stack<AdjacencyList>(prev, 1, 5);
+
+    EXPECT_EQ(path.top(), 1);
+    path.pop();
+    EXPECT_EQ(path.top(), 3);
+    path.pop();
+    EXPECT_EQ(path.top(), 4);
+    path.pop();
+    EXPECT_EQ(path.top(), 5);
+    path.pop();
+    EXPECT_TRUE(path.empty());
+
+    const auto path_vec = path_to_array<AdjacencyList>(prev, 1, 5);
+
+    EXPECT_EQ(path_vec.size(), 4);
+    EXPECT_EQ(path_vec[0], 1);
+    EXPECT_EQ(path_vec[1], 3);
+    EXPECT_EQ(path_vec[2], 4);
+    EXPECT_EQ(path_vec[3], 5);
+}
