dd5adb685ff5d12dd19ae89f301a5132da85b840
Yukino Hayakawa <tennencoll@gmail.com>
2023-01-02 20:12:35 +0800

    add runtime key value storage test

---
+++ TestRuntimeKeyValueStorage/Test.cpp
@@ -0,0 +1,27 @@
+﻿
+
+#include <gtest/gtest.h>
+
+#include <Usagi/Modules/Runtime/KeyValueStorage/OperatorReadRuntimeKeyValue.hpp>
+#include <Usagi/Modules/Runtime/KeyValueStorage/ServiceRuntimeKeyValueStorage.hpp>
+
+using namespace usagi;
+
+struct Runtime
+    : ServiceRuntimeKeyValueStorage
+{
+};
+
+TEST(RuntimeKeyValueStorage, OpReadTest)
+{
+    Runtime rt;
+    ServiceAccess<ServiceRuntimeKeyValueStorage> access(rt);
+
+    constexpr std::size_t val = 1234;
+    constexpr auto key = "size";
+
+    access.kv_storage().ensure<std::size_t>(key) = val;
+
+    OperatorReadRuntimeKeyValue<"size", std::size_t> op;
+    EXPECT_EQ(op(access, std::nullptr_t()), val);
+}
