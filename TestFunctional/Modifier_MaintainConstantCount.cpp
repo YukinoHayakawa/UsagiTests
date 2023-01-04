ade452dd79d70270cb775ff3c7bc769bc2a37ede
Yukino Hayakawa <tennencoll@gmail.com>
2023-01-04 19:42:45 +0800

    add tests for functional module

---
+++ TestFunctional/Modifier_MaintainConstantCount.cpp
@@ -0,0 +1,87 @@
+﻿#include <gtest/gtest.h>
+
+#include <Usagi/Entity/Component.hpp>
+#include <Usagi/Modules/Common/Functional/Algorithms/OperatorConstantValue.hpp>
+#include <Usagi/Modules/Common/Functional/Algorithms/OperatorCountEntities.hpp>
+#include <Usagi/Modules/Common/Functional/Modifiers/SystemMaintainConstantEntityCount.hpp>
+
+#include "../TestECSCommon/App.hpp"
+
+using namespace usagi;
+
+namespace 
+{
+USAGI_DECL_TAG_COMPONENT(CDeadTag);
+struct CExampleVal
+{
+    int val;
+};
+}
+
+struct Runtime { };
+
+using ArchetypeT = Archetype<CExampleVal>;
+
+struct Initializer
+{
+    int i = 0;
+
+    // todo: whether to allow the initializer to access db?
+    void operator()(ArchetypeT &archetype, auto &&rt, auto &&db)
+    {
+        archetype(C<CExampleVal>()).val = i++;
+    }
+};
+
+using SysMaintainAliveEntityCount = SystemMaintainConstantEntityCount<
+    CQuery<C<CExampleVal>, C<CDeadTag>>,
+    OperatorConstantValue<std::size_t, 12>,
+    ArchetypeT,
+    Initializer
+>;
+
+using TestMaintainEntityCount = TestApp<
+    C<CDeadTag, CExampleVal>,
+    Runtime,
+    SysMaintainAliveEntityCount
+>;
+
+TEST_F(TestMaintainEntityCount, Test)
+{
+    using CountTotal = OperatorCountEntities<CQuery<C<CExampleVal>>>;
+    using CountAlive = OperatorCountEntities<CQuery<C<CExampleVal>, C<CDeadTag>>>;
+    using CountDead = OperatorCountEntities<CQuery<C<CExampleVal, CDeadTag>>>;
+
+    auto db_access = db.create_access<ComponentAccessAllowAll>();
+
+    EXPECT_EQ(CountTotal()(runtime, db_access), 0);
+
+    // should spawn 12 alive entities
+    update_system<SysMaintainAliveEntityCount>();
+
+    EXPECT_EQ(CountTotal()(runtime, db_access), 12);
+    EXPECT_EQ(CountAlive()(runtime, db_access), 12);
+    EXPECT_EQ(CountDead()(runtime, db_access), 0);
+
+    auto range = db_access.view(C<CExampleVal>());
+
+    EXPECT_EQ(std::distance(range.begin(), range.end()), 12);
+
+    for(int i = 0; auto &&e: range)
+    {
+        EXPECT_EQ(e(C<CExampleVal>()).val, i++);
+        // kill the first two entities
+        if(i < 3) e.add_component(C<CDeadTag>());
+    }
+
+    EXPECT_EQ(CountTotal()(runtime, db_access), 12);
+    EXPECT_EQ(CountAlive()(runtime, db_access), 10);
+    EXPECT_EQ(CountDead()(runtime, db_access), 2);
+
+    // should spawn another two to replace the 
+    update_system<SysMaintainAliveEntityCount>();
+    
+    EXPECT_EQ(CountTotal()(runtime, db_access), 14);
+    EXPECT_EQ(CountAlive()(runtime, db_access), 12);
+    EXPECT_EQ(CountDead()(runtime, db_access), 2);
+}
---
+++ TestFunctional/Modifier_Replication.cpp
@@ -0,0 +1,70 @@
+﻿#include <Usagi/Modules/Common/Functional/Modifiers/SystemReplicateFillEntities.hpp>
+
+#include "../TestECSCommon/App.hpp"
+
+using namespace usagi;
+
+namespace
+{
+struct CReplicationOrigin
+{
+    EntityId id;
+};
+
+struct CExampleValue
+{
+    int value;
+};
+
+using SysReplicateValue = SystemReplicateFillEntities<
+    CQuery<C<CReplicationOrigin>>,
+    CReplicationOrigin,
+    C<CExampleValue>
+>;
+
+struct Runtime { };
+
+using EntityReplicationTest = TestApp<
+    C<CReplicationOrigin, CExampleValue>,
+    Runtime,
+    SysReplicateValue
+>;
+}
+
+TEST_F(EntityReplicationTest, ReplicateFillEntitiesTest)
+{
+    Archetype<CExampleValue> archetype_origin;
+
+    archetype_origin(C<CExampleValue>()).value = 1;
+    const auto id0 = db.insert(archetype_origin);
+    archetype_origin(C<CExampleValue>()).value = 2;
+    [[maybe_unused]]
+    const auto id1 = db.insert(archetype_origin);
+    archetype_origin(C<CExampleValue>()).value = 3;
+    const auto id2 = db.insert(archetype_origin);
+
+    Archetype<CReplicationOrigin> archetype_dest;
+    archetype_dest(C<CReplicationOrigin>()).id = id0;
+    db.insert(archetype_dest);
+    archetype_dest(C<CReplicationOrigin>()).id = id2;
+    db.insert(archetype_dest);
+
+    for(auto &&e : filtered_range(CQuery<C<CReplicationOrigin>>()))
+    {
+        EXPECT_FALSE(e.include(C<CExampleValue>()));
+    }
+
+    update_system<SysReplicateValue>();
+
+    auto range = filtered_range(CQuery<C<CReplicationOrigin>>());
+    auto iter = range.begin();
+
+    ASSERT_TRUE((*iter).include(C<CExampleValue>()));
+    EXPECT_EQ((*iter)(C<CExampleValue>()).value, 1);
+    ++iter;
+    ASSERT_TRUE((*iter).include(C<CExampleValue>()));
+    EXPECT_EQ((*iter)(C<CExampleValue>()).value, 3);
+    ++iter;
+
+    EXPECT_EQ(iter, range.end());
+}
